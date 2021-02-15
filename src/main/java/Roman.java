06df8116fd61a7599c39f63603547b72af0d49cc
christopher <christopher.zelezny@mailbox.org>
2021-02-15 17:37:25 +0100

    clean up

--- src/main/java/Roman.java
+++ src/main/java/Roman.java
@@ -2,23 +2,17 @@ public class Roman {
 
 
     public int toArabic(String romanNumber) {
-        int sum = 0;
-        int[] digits = new int[romanNumber.length()];
+        int[] digits = convertDigits(romanNumber);
+        handleSubtractiveForm(digits);
+        return sum(digits);
+    }
 
+    private int[] convertDigits(String romanNumber) {
+        int[] digits = new int[romanNumber.length()];
         for (int i = 0; i < romanNumber.length(); i++) {
             digits[i] = mapLiteral(romanNumber.charAt(i));
         }
-
-        for (int i = 0; i < romanNumber.length() - 1; i++) {
-            if (digits[i] < digits[i + 1]) {
-                digits[i] *= -1;
-            }
-        }
-
-        for (int i = 0; i < romanNumber.length(); i++) {
-            sum += digits[i];
-        }
-        return sum;
+        return digits;
     }
 
     private int mapLiteral(char literal) {
@@ -30,5 +24,21 @@ public class Roman {
             return 10;
         }
         throw new IllegalArgumentException("No valid roman char");
+    }
+
+    private void handleSubtractiveForm(int[] digits) {
+        for (int i = 0; i < digits.length - 1; i++) {
+            if (digits[i] < digits[i + 1]) {
+                digits[i] *= -1;
+            }
+        }
+    }
+
+    private int sum(int[] digits) {
+        int sum = 0;
+        for (int digit : digits) {
+            sum += digit;
+        }
+        return sum;
     }
 }
